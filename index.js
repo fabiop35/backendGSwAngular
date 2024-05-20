556022ad83e9c9e325e00f14319aec8b8d87e6c0
NAME <mail@gmail.com>
2024-05-19 19:53:25 -0500

    CH08 - Get Posts

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 const mongoose = require('mongoose');
 const url = 'mongodb://localhost/blog';
 const User = require('./models/user');
+const Post = require('./models/post');
+
 mongoose.set('strictQuery', true);
 app.use(bodyParser.json());
 
@@ -22,6 +24,28 @@ app.post('/api/user/login', (req, res) => {
           return res.status(200).json({
             status: 'failed',
             message: 'Login Failed'
+          })
+        }
+    
+    }).catch( (error) => {res.status(500).json(error);
+      
+    })
+  );
+});
+
+app.post('/api/post/getAllPost', (req, res) => {
+  mongoose.connect(`mongodb://localhost:27017/blog`).then(
+    Post.find( {}, [], { sort: { _id: -1}} ).then((docs) => {
+        console.log("--->Result: ", docs);
+        if(docs){
+         return res.status(200).json({
+           status: 'success',
+           data: docs
+         })
+        }else{
+          return res.status(404).json({
+            status: 'Not found',
+            message: 'Getting Post Failed'
           })
         }
     
